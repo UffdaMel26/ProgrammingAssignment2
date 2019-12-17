b228800cc5feab1de34f314a71e3166efd63ac0f
UffdaMel26 <57455667+UffdaMel26@users.noreply.github.com>
2019-12-16 21:58:43 -0800

    Week3 programming assignment
    completed functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to invert a matrix, store results and call-back result
 
-## Write a short comment describing this function
-
+## Function to store the inverse of a matrix (to call at another time)
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) m <<- solve
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
-## Write a short comment describing this function
-
+## Function to look up an inverted matrix, or calculate the inverted
+## matrix if it does not already exist.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
+
+
+
+
+  
+  
+  
